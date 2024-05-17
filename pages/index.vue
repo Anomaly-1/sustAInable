037c72e35403818bb1cd2ee59ae005f6893f88b5
Anomaly-1 <cognitivetech52@gmail.com>
2024-05-16 22:37:19 -0500

    Base Release

--- pages/index.vue
+++ pages/index.vue
@@ -54,8 +54,3 @@
 <style lang="scss">
 @import url('https://fonts.googleapis.com/css2?family=Averia+Libre:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&display=swap');
 </style>
-
-<script>
-import GoogleMapLoader from '~/components/GoogleMapLoader.vue';
-
-</script>
