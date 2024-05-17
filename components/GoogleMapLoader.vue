f56947e53c5d1f2004f4e345e3cdd1254800acad
Anomaly-1 <cognitivetech52@gmail.com>
2024-05-16 22:41:00 -0500

    Base Release

--- components/GoogleMapLoader.vue
+++ components/GoogleMapLoader.vue
@@ -6,7 +6,7 @@
 
 <script>
 import { onMounted, ref } from 'vue';
-import Loader from '@googlemaps/js-api-loader';
+import {Loader} from '@googlemaps/js-api-loader';
 
 const loader = new Loader({
   apiKey: googlemapsapikey,
